131cad8c307bca45c1cbe619000a5ce803c5142d
Duncan DeVore <duncan.devore@gmail.com>
2012-08-24 15:39:02 -0400

    Updated EmployeeSpec for salatDAO updates

--- backoffice-hr/src/test/scala/com/viridityenergy/backoffice/hr/model/EmployeeSpec.scala
+++ backoffice-hr/src/test/scala/com/viridityenergy/backoffice/hr/model/EmployeeSpec.scala
@@ -17,15 +17,16 @@ class EmployeeSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfter {
       info("As a programmer")
       info("I want to insure that an employee document is inserted")
       info("I want to insure that the employee document is read")
+      info("I want to insure that the employee document is updated")
 
     scenario("a new employee is inserted") {
 
       given("a new employee and a collection count of 0")
       assert(EmployeeDAO.collection.size === 0)
-      val employee1 = Employee(name = "Sean Walsh", age = Option(47), annualSalary = Option(55000))
+      val newEmployee = Employee(name = "Sean Walsh", age = Option(47), annualSalary = Option(55000))
 
-      when("the employee is inserted")
-      val _id = EmployeeDAO.insert(employee1)
+      when("the new employee is inserted")
+      val newEmployeeId = EmployeeDAO.insert(newEmployee)
 
       then("the colletion count should be 1")
       assert(EmployeeDAO.collection.size === 1)
@@ -35,18 +36,39 @@ class EmployeeSpec extends FeatureSpec with GivenWhenThen with BeforeAndAfter {
 
       given("a new employee and a collection count of 0")
       assert(EmployeeDAO.collection.size === 0)
-      val employee1 = Employee(name = "Sean Walsh", age = Option(47), annualSalary = Option(55000))
+      val newEmployee = Employee(name = "Duncan DeVore", age = Option(47), annualSalary = Option(55000))
 
-      when("the employee is inserted and read by id")
-      val _id = EmployeeDAO.insert(employee1)
-      val employee = EmployeeDAO.findOneById(_id.get)
+      when("the new employee is inserted and the inserted employee is found by the new employee id")
+      val newEmployeeId = EmployeeDAO.insert(newEmployee)
+      val insertedEmployee = EmployeeDAO.findOneById(newEmployeeId.get)
 
-      then("the read employees id should equal the _id resulting from the insert")
-      assert (employee.get.id === _id.get)
+      then("the inserted employee id should equal the new employee id")
+      assert (insertedEmployee.get.id === newEmployeeId.get)
     }
 
-    // scenario("Update Employee") (pending)
+    scenario("a new employee is inserted then read then updated") {
 
-    // scenario("Delete Employee") (pending)
+      given("a new employee and a collection count of 0")
+      assert(EmployeeDAO.collection.size === 0)
+      val newEmployee = Employee(name = "Craig Stewart", age = Option(43), annualSalary = Option(55000))
+
+      when("the new employee is inserted and the inserted employee is found by the new employee id")
+      val newEmployeeId = EmployeeDAO.insert(newEmployee)
+      val insertedEmployee = EmployeeDAO.findOneById(newEmployeeId.get)
+
+      then("the inserted employee id should equal the new employee id")
+      assert (insertedEmployee.get.id === newEmployeeId.get)
+
+      when("the inserted employee is copied and the annualSalary is updated")
+      val updatedEmployee = insertedEmployee.get.copy(annualSalary = Option(650000))
+      EmployeeDAO.update(MongoDBObject("_id" -> insertedEmployee.get.id), updatedEmployee, false, false, new WriteConcern)
+
+      then("the fetched updated employee id equals the new employee id and the salary has been updated")
+      val fetchedUpdatedEmployee = EmployeeDAO.findOneById(insertedEmployee.get.id)
+      assert (fetchedUpdatedEmployee.get.id === newEmployeeId.get)
+      assert (fetchedUpdatedEmployee.get.annualSalary === Option(650000))
+    }
+
+    scenario("Delete Employee") (pending)
   }
 }
--- backoffice-hr/src/test/scala/com/viridityenergy/backoffice/hr/model/EmployeeSuite.scala
+++ backoffice-hr/src/test/scala/com/viridityenergy/backoffice/hr/model/EmployeeSuite.scala
@@ -1,26 +1,26 @@
-package com.viridityenergy.backoffice.hr.model
+// package com.viridityenergy.backoffice.hr.model
 
-import com.mongodb.casbah.Imports._
-import org.scalatest.FunSuite
+// import com.mongodb.casbah.Imports._
+// import org.scalatest.FunSuite
 
-class EmployeeSuite() extends FunSuite {
+// class EmployeeSuite() extends FunSuite {
 
-  test("Create Employee - EmployeeSuite") {
+//   test("Create Employee - EmployeeSuite") {
 
-    // create new employee
-    val employee1 = Employee(name = "Duncan DeVore", age = Option(47), annualSalary = Option(50000))
+//     // create new employee
+//     val employee1 = Employee(name = "Duncan DeVore", age = Option(47), annualSalary = Option(50000))
 
-    // insert employee
-    val _id = EmployeeDAO.insert(employee1)
+//     // insert employee
+//     val _id = EmployeeDAO.insert(employee1)
 
-    // fetch employee just inserted
-    val employee = EmployeeDAO.findOneById(_id.get)
+//     // fetch employee just inserted
+//     val employee = EmployeeDAO.findOneById(_id.get)
 
-    // verify origional employee id = fetched employee's id
-    assert (employee.get.id === _id.get)
+//     // verify origional employee id = fetched employee's id
+//     assert (employee.get.id === _id.get)
 
-    println("BEFORE DROP " + EmployeeDAO.collection)
-    EmployeeDAO.collection.drop
-    println("AFTER DROP " + EmployeeDAO.collection)
-  }
-}
+//     println("BEFORE DROP " + EmployeeDAO.collection)
+//     // EmployeeDAO.collection.drop
+//     println("AFTER DROP " + EmployeeDAO.collection)
+//   }
+// }
